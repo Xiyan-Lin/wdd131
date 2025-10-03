d4987c48add588c356991db38bf6700ed345b203
Xiyan Lin <xlin@byupathway.edu>
2025-10-04 00:23:59 +0800

    upd_08

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
 // Dynamically set current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 
 // Dynamically set last modified date
-document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
